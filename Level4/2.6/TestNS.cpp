dcf489d91ad486091ce2842424ace55e5b88d4a9
Francis <francis.lin@tksping.com>
2023-12-10 19:39:02 +0800

    revise code

---
+++ Level4/2.6/TestNS.cpp
@@ -0,0 +1,17 @@
+// TestNS.cpp
+//
+// Program that will not compile. Solve by 3 uses of 
+// C++ namespaces.
+//
+// DJD
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "why will it not compile?? " << endl;// not use right name space
+
+	return 0;
+}
